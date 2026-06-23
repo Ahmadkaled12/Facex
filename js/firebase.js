fd47d494e1e03996883848313818af9e89f8339b
Ahmadkaled12 <ahmadsupze@gmail.com>
2026-06-23 15:32:00 +0300

    Update Firebase configuration and app ID

--- js/firebase.js
+++ js/firebase.js
@@ -1,35 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCVJclfpEp9Jo9879C3RfZKiEdBUibwrx",
+  apiKey: "AIzaSyCVJclfpEp9Jo9879C3RfZKiEdBUibwrxI",
   authDomain: "ahmad-662ee.firebaseapp.com",
   projectId: "ahmad-662ee",
   storageBucket: "ahmad-662ee.firebasestorage.app",
   messagingSenderId: "740891153734",
-  appId: "1:740891153734:web:f43b86effbb4d70a618147"
-};
-
-// تحميل Firebase بشكل مضمون
-const script1 = document.createElement("script");
-script1.src = "https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js";
-document.head.appendChild(script1);
-
-const script2 = document.createElement("script");
-script2.src = "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth-compat.js";
-document.head.appendChild(script2);
-
-script2.onload = () => {
-
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-
-// 🔥 أهم شيء: تعريف عالمي مضمون
-window.loginAccount = function(email, password){
-return auth.signInWithEmailAndPassword(email, password);
-};
-
-window.createAccount = function(email, password){
-return auth.createUserWithEmailAndPassword(email, password);
-};
-
-console.log("Firebase Loaded ✔");
+  appId: "1:740891153734:web:f43b86effbb4b982618147",
+  measurementId: "G-K0B0KEN9EQ"
 };
